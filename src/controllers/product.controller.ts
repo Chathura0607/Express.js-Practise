6534531dd737e7fba2d156bb0808427d8e43ca6b
chathura <chathuhiru45@gmail.com>
2025-07-05 15:10:03 +0530

    crud operation done

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -33,10 +33,50 @@ export const saveProduct = (req: Request, res: Response) => {
 }
 
 export const getProduct = (req: Request, res: Response) => {
+    const productId = parseInt(req.params.id);
+    if (isNaN(productId)) {
+        res.status(400).json({error: 'Invalid product ID'}
+        );
+        return;
+    }
+    const product = productService.getProductById(productId);
+    if (!product) {
+        res.status(404).json({error: 'Product not found'});
+        return;
+    }
+    res.status(200).json(product);
 }
 
 export const updateProduct = (req: Request, res: Response) => {
+    const productId = parseInt(req.params.id);
+    if (isNaN(productId)) {
+        res.status(400).json({error: 'Invalid product ID'}
+        );
+        return;
+    }
+    const updatedData = req.body;
+    const updatedProduct = productService.updateProduct(productId, updatedData);
+    if (!updatedProduct) {
+        res.status(400).json({error: 'Failed to update product'}
+        );
+        return;
+    }
+    res.status(200).json(updatedProduct);
 }
 
 export const deleteProduct = (req: Request, res: Response) => {
+    const productId = parseInt(req.params.id);
+    if (isNaN(productId)) {
+        res.status(400).json({error: 'Invalid product ID'}
+        );
+        return;
+    }
+    const deleteProduct = productService.deleteProduct(productId);
+    if (!deleteProduct) {
+        res.status(404).json({error: 'Product not found'}
+        );
+        return;
+    }
+    res.status(200).json({message: 'Product deleted successfully'});
+
 }
