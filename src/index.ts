d8bea6be143be2902d0e1fafad520710741245d6
chathura <chathuhiru45@gmail.com>
2025-07-05 10:00:50 +0530

    Temp commit before rebase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import dotenv from "dotenv";
 
-// 2. Define the application port
-const port = 3000;
+dotenv.config(); // Load environment variables from .env file
+
+// 1. Define the application port
+const port = process.env.PORT || 3000; // Access the port
 
 // 4. Instruct the express app to listen on port 3000
 app.listen(port, () => {
